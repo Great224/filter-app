c90d0818c2e021e5785a2fa622efc9e06879a995
unknown <Greatibe224@gmail.com>
2022-10-04 22:27:53 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React,{useState}from 'react';
 import './App.css';
+import {arry1} from "./components/List"
 
 function App() {
+  const [search, setSearch] = useState("");
+  console.log(search)
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>FILTER APP</h1>
+   <div>
+    <input type="text" placeholder="SEARCH...." onChange={e=> setSearch(e.target.value)}/>
+   </div>
     </div>
+    {arry1.filter((m) =>
+    m.firstName.toLowerCase().includes(search) ||
+    m.firstName.toUpperCase().includes(search) ||
+    m.lastName.toLowerCase().includes(search) ||
+    m.lastName.toUpperCase().includes(search)
+    ).map((m) =>(
+      <div key={m.id} className="table">
+      <p>{m.firstName}</p>
+      <p>{m.lastName}</p>
+      </div>
+))}
+    </>
   );
 }
 
